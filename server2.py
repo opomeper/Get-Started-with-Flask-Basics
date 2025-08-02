04108439e9a05e51770b9157a8f809f43663ff65
opomeper <saurojoemer14@gmail.com>
2025-08-02 23:08:04 +0800

    Added 2nd lab server2.py

---
+++ server2.py
@@ -0,0 +1,14 @@
+""" This file is part of the Python Lab project. "Building an API with Flask: Route Creation, Error Handling, and HTTP Requests" """
+# Import the Flask class from the flask module
+from flask import Flask
+
+# Create an instance of the Flask class, passing in the name of the current module
+app = Flask(__name__)
+
+# Define a route for the root URL ("/")
+@app.route("/")
+def index():
+    # Function that handles requests to the root URL
+    # Return a plain text response
+    return "hello world"
+
